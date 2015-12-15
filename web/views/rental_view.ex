5fd2483cf9d67e2674e96a855fa606eb32134547
Leeroy Ding <i@leeroy.im>
2015-12-15 16:13:57 +0000

    Removing `render/2` implementations.

--- web/views/rental_view.ex
+++ web/views/rental_view.ex
@@ -3,21 +3,4 @@ defmodule SuperRentalsBackend.RentalView do
 
   attributes [:title, :owner, :city, :type, :bedrooms, :image]
   
-  def render("index.json", %{rentals: rentals}) do
-    %{data: render_many(rentals, SuperRentalsBackend.RentalView, "rental.json")}
-  end
-
-  def render("show.json", %{rental: rental}) do
-    %{data: render_one(rental, SuperRentalsBackend.RentalView, "rental.json")}
-  end
-
-  def render("rental.json", %{rental: rental}) do
-    %{id: rental.id,
-      title: rental.title,
-      owner: rental.owner,
-      city: rental.city,
-      type: rental.type,
-      bedrooms: rental.bedrooms,
-      image: rental.image}
-  end
 end
