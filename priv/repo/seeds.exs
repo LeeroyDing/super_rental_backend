faaccddb75b8a6ebef7e8516ab8e4d0f2be65882
Leeroy Ding <i@leeroy.im>
2015-12-15 13:23:13 +0000

    Add repo seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,33 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias SuperRentalsBackend.Repo
+alias SuperRentalsBackend.Rental
+
+[
+  %Rental{
+    title: "Grand Old Mansion",
+    owner: "Veruca Salt",
+    city: "San Francisco",
+    type: "Estate",
+    bedrooms: 15,
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+  },
+  %Rental{
+    title: "Urban Living",
+    owner: "Mike Teavee",
+    city: "Seattle",
+    type: "Condo",
+    bedrooms: 1,
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+  },
+  %Rental{
+    title: "Downtown Charm",
+    owner: "Violet Beauregarde",
+    city: "Portland",
+    type: "Apartment",
+    bedrooms: 3,
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+  }] |> Enum.each(&Repo.insert!(&1))
+
