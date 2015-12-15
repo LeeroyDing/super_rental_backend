63a6dc2bf38eec0a1ec1f24198fa008c5edd4228
Leeroy Ding <i@leeroy.im>
2015-12-15 13:11:50 +0000

    Modify web/route.ex

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,14 @@
 defmodule SuperRentalsBackend.Router do
   use SuperRentalsBackend.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", SuperRentalsBackend do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api
 
-    get "/", PageController, :index
+    resources "/rentals", RentalController, except: [:new, :edit]
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", SuperRentalsBackend do
-  #   pipe_through :api
-  # end
 end
